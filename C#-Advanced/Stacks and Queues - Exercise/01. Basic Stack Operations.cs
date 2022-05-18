b2e23a073265c9a7b520cb617e87747a70762627
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-05-18 14:45:52 +0300

    Update 01. Basic Stack Operations.cs

--- C#-Advanced/Stacks and Queues - Exercise/01. Basic Stack Operations.cs
+++ C#-Advanced/Stacks and Queues - Exercise/01. Basic Stack Operations.cs
@@ -1,12 +1,52 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Stacks_and_Queues___Exercise
+namespace _01._Basic_Stack_Operations
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] parms = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = parms[0];
+            int s = parms[1];
+            int x = parms[2];
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int element = input[i];
+                stack.Push(element);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count>0)
+            {
+                if (stack.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine($"{stack.Min()}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
         }
     }
 }
