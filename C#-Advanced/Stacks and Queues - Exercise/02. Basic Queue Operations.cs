aa6728999be89ac9e133a2f1f4d0fc53c38f9197
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-05-18 14:50:30 +0300

    Update 02. Basic Queue Operations.cs

--- C#-Advanced/Stacks and Queues - Exercise/02. Basic Queue Operations.cs
+++ C#-Advanced/Stacks and Queues - Exercise/02. Basic Queue Operations.cs
@@ -1,12 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stacks_and_Queues___Exercise
+namespace _01._Basic_Stack_Operations
 {
-    internal class Class1
+    internal class Program
+
     {
+        static void Main(string[] args)
+        {
+            int[] parms = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = parms[0];
+            int s = parms[1];
+            int x = parms[2];
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> stack = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int element = input[i];
+                stack.Enqueue(element);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Dequeue();
+            }
+            if (stack.Count > 0)
+            {
+                if (stack.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine($"{stack.Min()}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
     }
 }
