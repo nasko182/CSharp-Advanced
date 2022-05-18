fcf4c787a85ef8024fd22af9901f141cff2ded41
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-05-18 16:23:05 +0300

    Update 04. Fast Food.cs

--- C#-Advanced/Stacks and Queues - Exercise/04. Fast Food.cs
+++ C#-Advanced/Stacks and Queues - Exercise/04. Fast Food.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stacks_and_Queues___Exercise
+namespace _01._Basic_Stack_Operations
 {
-    internal class Class1
+    internal class Program
+
     {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+            int[] quantityOfOrder = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>(quantityOfOrder.Reverse());
+            Console.WriteLine(quantityOfOrder.Max());
+            if (queue.Sum() <= quantityOfFood)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                    Console.Write($"Orders left:");
+                for (int i = 0; i < quantityOfOrder.Length; i++)
+                {
+                        if (queue.Sum() > quantityOfFood)
+                        {
+                            Console.Write($" {queue.Dequeue()}");
+                            
+                        }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
