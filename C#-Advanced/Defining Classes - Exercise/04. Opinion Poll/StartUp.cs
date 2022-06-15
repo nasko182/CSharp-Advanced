a9946a8369540908e1b26af23ef885875a6bc1de
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-06-15 21:45:48 +0300

    Add files via upload

---
+++ C#-Advanced/Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                if (age>30)
+                {
+                    people.Add(new Person(name, age));
+                }
+            }
+
+            foreach (Person person in people.OrderBy(x=>x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
