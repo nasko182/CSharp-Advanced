64e909997f24169f98e2c7d089eae7e969139752
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-06-15 22:21:17 +0300

    Add files via upload

---
+++ C#-Advanced/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class DateModifier
+    {
+        public DateModifier(string firstData, string lastData)
+        {
+            FirstData = DateTime.Parse( firstData);
+            LastData = DateTime.Parse( lastData);
+        }
+
+        public DateTime FirstData { get; set; }
+
+        public DateTime LastData { get; set; }
+
+        public int GetDiff()
+        {
+            return Math.Abs((int)(FirstData - LastData).TotalDays);
+        }
+
+    }
+}
---
+++ C#-Advanced/Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier(firstDate, secondDate);
+
+            Console.WriteLine(dateModifier.GetDiff());
+        }
+    }
+}
