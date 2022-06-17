51c7cf49ad33f64faf36b13fe15dd6e72ccbc12d
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-06-17 14:47:32 +0300

    Add files via upload

---
+++ C#-Advanced/Generics-Lab/01. Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    internal class Box<T>
+    {
+        private Stack<T> stack = new Stack<T>();
+
+        public void Add(T element)
+        {
+            stack.Push(element);
+        }
+        public T Remove()
+        {
+            return stack.Pop();
+        }
+        public int Count 
+        { 
+            get
+            {
+                return stack.Count;
+            } 
+        }
+    }
+}
