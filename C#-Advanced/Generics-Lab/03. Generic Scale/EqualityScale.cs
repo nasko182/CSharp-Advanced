bd49e050f7919359fcf4830665fd7bd9debb488e
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-06-17 15:10:41 +0300

    Add files via upload

---
+++ C#-Advanced/Generics-Lab/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+       public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        private T left;
+        private T right;
+
+        public bool AreEqual()
+            
+        {
+            return this.left.Equals(this.right);
+        }
+    }
+}
