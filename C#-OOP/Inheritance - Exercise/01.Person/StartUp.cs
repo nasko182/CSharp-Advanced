f07d110f8c402d8359453772691e815ca6163343
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-09 22:13:38 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Exercise/01.Person/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Humans
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+
+        }
+
+    }
+
+   
+}
