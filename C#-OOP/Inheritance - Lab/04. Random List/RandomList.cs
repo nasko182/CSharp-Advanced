c607b2e7074f7125eb9447e11c00c011aa10016d
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-09 15:58:48 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var random = new Random();
+            var item = this[random.Next(0, this.Count)];
+            this.Remove(item);
+            return item;
+            
+        }
+    }
+}
