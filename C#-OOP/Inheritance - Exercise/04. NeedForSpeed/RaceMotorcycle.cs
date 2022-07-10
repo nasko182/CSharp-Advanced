cfee2e017f570c12ccf859a3b6691c685f71f26d
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-10 14:18:46 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Exercise/04. NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double DefaultRaceMotorcycleFuelConsumption = 8;
+
+        public RaceMotorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+        public override double FuelConsumption
+            => DefaultRaceMotorcycleFuelConsumption;
+    }
+}
