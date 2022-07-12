fe005714557135d1f94ede1c2aa177a8ca8bb4f2
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-12 10:37:00 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Exercise/05.Restaurant/Tea.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant
+{
+    public class Tea : HotBeverage
+    {
+        public Tea(string name, decimal price, double mililiters)
+            : base(name, price, mililiters)
+        {
+
+        }
+    }
+}
