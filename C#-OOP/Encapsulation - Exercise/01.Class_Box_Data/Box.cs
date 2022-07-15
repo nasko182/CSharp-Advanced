2e03123f8d41c97eb09c8a30fed3aaddef0f05c5
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-15 13:44:01 +0300

    Add files via upload

---
+++ C#-OOP/Encapsulation - Exercise/01.Class_Box_Data/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Box_Data
+{
+    public class Box
+    {
+        public Box(double length, double width,double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get => length;
+            set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Length");
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width; 
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width");
+                }
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height; 
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Height * this.Width;
+        }
+
+        public double LateralSurfceArea()
+        {
+            return 2 * this.Height * (this.Length + this.Width);
+        }
+
+        public double Volume()
+        {
+            return this.Length* this.Width*this.Height;
+        }
+    }
+}
---
+++ C#-OOP/Encapsulation - Exercise/01.Class_Box_Data/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class_Box_Data
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double widht = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                var box = new Box(length, widht, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"{ae.Message} cannot be zero or negative.");
+            }
+        }
+    }
+}
