4237f20c0ff3cb235b4c1dc15054e939cb0e2eff
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-16 15:31:32 +0300

    Add files via upload

---
+++ C#-OOP/Inheritance - Exercise/06. Animals/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+       
+        protected Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+            this.Type = "Animal";
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string  Gender { get; set; }
+
+        public string Type { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return "";
+        }
+        
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(this.Type);
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.Append(ProduceSound());
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#-OOP/Inheritance - Exercise/06. Animals/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            string command;
+            while ((command = Console.ReadLine())!="Beast!")
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                string gender = input[2];
+                if (age<0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                if (gender!="Male" && gender!="Female" )
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                if (command == "Cat")
+                {
+                    var animal = new Cat(name, age, gender);
+                    animals.Add(animal);
+                }
+                else if (command == "Dog")
+                {
+                    var animal = new Dog(name, age, gender);
+                    animals.Add(animal);
+                }
+                else if (command == "Frog")
+                {
+                    var animal = new Frog(name, age, gender);
+                    animals.Add(animal);
+                }
+                else if (command == "Kitten")
+                {
+                    var animal = new Kitten(name, age);
+                    animals.Add(animal);
+                }
+                else if (command == "Tomcat")
+                {
+                    var animal = new Tomcat(name, age);
+                    animals.Add(animal);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+
+            }
+
+                foreach(var animal in animals)
+                {
+                    Console.WriteLine(animal.ToString());
+                }
+
+        }
+    }
+}
