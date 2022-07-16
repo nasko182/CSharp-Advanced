5643e4b92e3f9e9e17f536e7dbb56fd2fbfc6393
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-16 21:59:44 +0300

    Add files via upload

---
+++ C#-OOP/Encapsulation - Exercise/03.ShoppingSpree/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Product
+{
+    private string name;
+    private double cost;
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+            this.name = value;
+        }
+    }
+    public double Cost
+    {
+        get
+        {
+            return this.cost;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Money cannot be negative");
+            }
+            this.cost = value;
+        }
+    }
+
+    public Product(string name, double cost)
+    {
+        Name = name;
+        Cost = cost;
+    }
+}
