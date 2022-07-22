27d3fef649d9e550b70affacedd3f63a12b61fd7
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-22 20:12:53 +0300

    Add files via upload

---
+++ C#-OOP/Polymorphism - Lab/03.Shapes/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; protected set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ C#-OOP/Polymorphism - Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Circle(3.2);
+            Console.WriteLine(shape.Draw());
+            shape = new Rectangle(6, 5);
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
