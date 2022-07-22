d1bf066d7a448feebf40b09861d928e2c9b2cd10
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-22 21:08:01 +0300

    Add files via upload

---
+++ C#-OOP/Polymorphism - Exercise/02._VehiclesExtension/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿public abstract class Vehicle
+{
+    private double fuelQuantity;
+    private double consumprionPerKilometer;
+    private double tankCapacity;
+
+    public double FuelQuantity
+    {
+        get { return fuelQuantity; }
+        set { fuelQuantity = value; }
+    }
+
+    public double ConsumprionPerKilometer
+    {
+        get { return consumprionPerKilometer; }
+        set { consumprionPerKilometer = value; }
+    }
+
+    public double TankCapacity
+    {
+        get { return tankCapacity; }
+        set
+        { tankCapacity = value; }
+    }
+
+
+    public abstract void Drive(double distance);
+    public abstract void Refuel(double liters);
+}
