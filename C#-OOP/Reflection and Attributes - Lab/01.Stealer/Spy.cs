6cb04ea999163e2c5bfa4118a99684deab5b43db
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-27 20:15:21 +0300

    Add files via upload

---
+++ C#-OOP/Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsNames)
+        {
+            StringBuilder sb = new StringBuilder();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type type = assembly.GetType(className);
+            var classInstance = Activator.CreateInstance(type);
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+            FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (string fieldName in fieldsNames)
+            {
+                var currentFileld = fields.FirstOrDefault(f => f.Name == fieldName);
+                sb.AppendLine($"{currentFileld.Name} = {currentFileld.GetValue(classInstance)}");
+            }
+
+
+            return sb.ToString();
+        }
+    }
+}
