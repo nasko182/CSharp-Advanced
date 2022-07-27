740f736a0082bfe4247ee8a15dd2fc5796b22ae5
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-27 23:03:29 +0300

    Add files via upload

---
+++ C#-OOP/Reflection and Attributes - Lab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    [Author("George")]
+    public class StartUp
+    {
+        [Author("George")]
+        [Author("Pesho")]
+        static void Main()
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ C#-OOP/Reflection and Attributes - Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+            var methods = type.GetMethods(BindingFlags.Public|BindingFlags.NonPublic|BindingFlags.Instance|BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(a=>a.AttributeType==typeof(AuthorAttribute)))
+                {
+                    foreach (AuthorAttribute attribute in method.GetCustomAttributes(false))
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
