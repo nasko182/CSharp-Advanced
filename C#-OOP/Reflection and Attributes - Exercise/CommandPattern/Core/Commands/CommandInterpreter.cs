ed3bfbb6cb27e2a1049c2b9f7c6bcdddbaf3e48e
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-07-28 15:18:50 +0300

    Add files via upload

---
+++ C#-OOP/Reflection and Attributes - Exercise/CommandPattern/Core/Commands/CommandInterpreter.cs
@@ -0,0 +1,26 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core.Commands
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string name = args.Split(' ')[0];
+            object[] cmdArgs = args.Split(' ').Skip(1).ToArray();
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type type = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == name+"Command"&& t.GetInterfaces().Any(i=>i==typeof(ICommand)));
+            MethodInfo method = type.GetMethod("Execute");
+            object cmdInstance = Activator.CreateInstance(type);
+            string result = (string)method.Invoke(cmdInstance, new object[] {cmdArgs});
+            return result;
+        } 
+    }
+}
